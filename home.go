a4da12e0c0f88119411cef0d15a967ecab1b28e1
laksh <lakshtaneja77@gmail.com>
2024-05-27 00:16:23 +0530

    fix: update currency code input in home.go

--- home.go
+++ home.go
@@ -7,7 +7,14 @@ import (
 )
 
 func main() {
-	rate, err := api.GetRate("ETH")
+	var code string
+
+	fmt.Println("Welcome to Crypto Price")
+	fmt.Println("=========================")
+	fmt.Println("Enter Currency code")
+	fmt.Scanf("%s", &code)
+
+	rate, err := api.GetRate(code)
 	if err == nil {
 		fmt.Printf("Rate for %s is %.2f USD\n", rate.Currency, rate.Price)
 	} else {
